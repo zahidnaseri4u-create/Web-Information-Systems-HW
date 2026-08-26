60bea80045cea18b1744c3f32dc289842de197dc
Zahid Nasiri <zahidnaseri4u@gmail.com>
2026-08-26 08:44:14 +0430

    Questions and answers
    Added answers to questions regarding class creation, inheritance, and method access.

--- Lab 1/questions_with_answers.php
+++ Lab 1/questions_with_answers.php
@@ -1,1 +1,37 @@
+Part C
 
+How many classes did you create?
+Answer: 1
+
+How many objects did you create?
+Answer: 2
+
+
+Part D
+
+Does $account1->balance work?
+Answer: No.
+
+Why?
+Answer: Because balance is private. We can only use it inside the BankAccount class.
+
+
+Part F
+
+Question 1: Which class is the parent class?
+Answer: Person
+
+Question 2: Which class is the child class?
+Answer: Student
+
+Question 3: Which keyword creates the inheritance relationship?
+Answer: extends
+
+Question 4: The introduce() method was written inside which class?
+Answer: Person
+
+Question 5: Can the $student1 object call introduce()?
+Answer: Yes
+
+Question 6: Why can $student1 use introduce() even though it is not written inside the Student class?
+Answer: Because Student gets the methods from the Person class.
